8cdbac8fa7eba6872934d06b81c77f98295fcfec
pambrose <pambrose@mac.com>
2022-03-04 22:13:41 -0800

    Upgrade to ktor 2.0

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 VERSION=0.1.0
 
-default: build-all
+default: versioncheck
 
 build-all: clean stage
 
@@ -31,4 +31,4 @@ versioncheck:
 	./gradlew dependencyUpdates
 
 upgrade-wrapper:
-	./gradlew wrapper --gradle-version=6.8.3 --distribution-type=bin
+	./gradlew wrapper --gradle-version=7.4 --distribution-type=bin
--- src/main/kotlin/com/github/pambrose/Application.kt
+++ src/main/kotlin/com/github/pambrose/Application.kt
@@ -3,37 +3,37 @@ package com.github.pambrose
 import com.github.pambrose.Page.generatePage
 import com.github.pambrose.Presentation.Companion.presentations
 import com.github.pambrose.common.response.respondWith
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.http.content.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.plugins.*
+import io.ktor.server.routing.*
 import org.slf4j.event.Level
 
 internal val staticRoots = listOf("public", "assets", "css", "dist", "js", "plugin")
 
 fun Application.module(testing: Boolean = false) {
 
-    install(CallLogging) { level = Level.INFO }
+  install(CallLogging) { level = Level.INFO }
 
-    install(DefaultHeaders) { header("X-Engine", "Ktor") }
+  install(DefaultHeaders) { header("X-Engine", "Ktor") }
 
-    install(Compression) {
-        gzip { priority = 1.0 }
-        deflate { priority = 10.0; minimumSize(1024) /* condition*/ }
+  install(Compression) {
+    gzip { priority = 1.0 }
+    deflate { priority = 10.0; minimumSize(1024) /* condition*/ }
+  }
+
+  routing {
+    staticRoots.forEach {
+      static("/$it") { resources(it) }
     }
 
-    routing {
-        staticRoots.forEach {
-            static("/$it") { resources(it) }
+    presentations.forEach { presentation ->
+      get(presentation.key) {
+        respondWith {
+          generatePage(presentation.value)
         }
-
-        presentations.forEach { presentation ->
-            get(presentation.key) {
-                respondWith {
-                    generatePage(presentation.value)
-                }
-            }
-        }
+      }
     }
+  }
 }
 
